50cc9e5876895c0580c5718424886026b3e17c41
Amanda Engel <amandae@ciandt.com>
2026-05-27 15:43:41 -0300

    Inclusão do manifest + novos registros

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'maps-v1';
+const STATIC = [
+  '/mes-photos/',
+  '/mes-photos/maps.html',
+  '/mes-photos/index.html',
+  '/mes-photos/map.js',
+  '/mes-photos/styles.css',
+  '/mes-photos/auth-social.css',
+  '/mes-photos/city-styles.css',
+  '/mes-photos/auth.js',
+  '/mes-photos/social.js',
+  '/mes-photos/firebase-config.js',
+  '/mes-photos/icons/icon-192.png',
+  '/mes-photos/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(STATIC))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
